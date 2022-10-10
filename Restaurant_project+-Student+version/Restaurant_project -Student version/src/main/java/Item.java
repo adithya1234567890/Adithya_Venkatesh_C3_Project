52b5270d1340017f2a08a7c754f48d608b395d23
adithya1234567890 <115413790+adithya1234567890@users.noreply.github.com>
2022-10-10 10:32:23 +0530

    Part 2: Solution
    All methods are implemented and all test cases are passed. Repeated code refactored is also done.

---
+++ Restaurant_project+-Student+version/Restaurant_project -Student version/src/main/java/Item.java
@@ -0,0 +1,22 @@
+public class Item {
+    private String name;
+    private int price;
+
+    public Item(String name1, int price1) {
+        name = name1;
+        price = price1;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+
+    @Override
+    public String toString(){
+        return  name + ":"
+                + price
+                + "\n"
+                ;
+    }
+}
